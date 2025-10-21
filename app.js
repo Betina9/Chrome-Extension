d90ae731f39205ee1152def6b90c4fce014b34a4
Betina9 <ob042025@gmail.com>
2025-10-21 10:58:34 +0200

    lagt til liste når en bruker skriver

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  const inputValue = inputEl.value;
-  myLeads.push(inputValue);
-
+  myLeads.push(inputEl.value);
   console.log(myLeads);
-  inputEl.value = "";
 });
+
+function renderLeads() {
+  ulEl.innerHTML = "";
+
+  for (let i = 0; i < myLeads.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = myLeads[i];
+    ulEl.append(li);
+  }
+}
+renderLeads();
