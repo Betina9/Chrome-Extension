13974e4705c3636637b1c5adfc5e2065c70d58b7
Betina9 <ob042025@gmail.com>
2025-10-22 14:15:39 +0200

    ferdig chrome extension

--- app.js
+++ app.js
@@ -4,30 +4,25 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
   myLead = leadsFromLocalStorage;
-  renderLead();
+  render(myLead);
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-  console.log("The delete button was clicked!");
-  localStorage.clear();
-  myLead = [];
-  renderLead();
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLead.push(tabs[0].url);
+    localStorage.setItem("myLead", JSON.stringyfy(myLead));
+    render(myLead);
+  });
 });
 
-inputBtn.addEventListener("click", function () {
-  myLead.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLead", JSON.stringify(myLead));
-  renderLead();
-});
-
-function renderLead() {
+function render(lead) {
   let listItems = "";
 
-  for (let i = 0; i < myLead.length; i++) {
+  for (let i = 0; i < lead.length; i++) {
     listItems += `
       <li>
         <a target='_blank' href='${myLead[i]}'>
@@ -37,3 +32,17 @@ function renderLead() {
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  console.log("The delete button was clicked!");
+  localStorage.clear();
+  myLead = [];
+  render(myLead);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLead.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLead", JSON.stringify(myLead));
+  render(myLead);
+});
